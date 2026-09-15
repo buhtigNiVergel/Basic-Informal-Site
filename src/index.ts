289ca18aa69112b0307febb0c2138b48862a8cf1
yomsk <moyamoy.vergelbscs2023@gmail.com>
2026-09-15 11:53:14 +0800

    finish project

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,58 @@
 import http from 'node:http'
 import { readFile } from 'node:fs'
 
+const errResponse = (res : http.ServerResponse<http.IncomingMessage>) => {
+    res.statusCode = 500;
+    res.end("Error reading")
+}
+
+const setStatusCodeAndHeader = (res : http.ServerResponse<http.IncomingMessage>) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+}
+
 const server = http.createServer((req , res) => {
     if(req.url === '/' && req.method === 'GET'){
         readFile('./index.html', (err, data) => {
             if (err) {
-                res.statusCode = 500;
-                res.end("Error reading index.html");
+                errResponse(res)
                 return;
             }
 
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "text/html");
+            setStatusCodeAndHeader(res)
             res.end(data)
         })
-    }else if(req.url === '/about.html' && req.method === 'GET'){
+    }else if(req.url === '/about' && req.method === 'GET'){
         readFile('./about.html', (err, data) => {
             if(err){
-                 res.statusCode = 500;
-                res.end("Error reading index.html");
+                errResponse(res)
                 return;
             }
 
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "text/html");
+            setStatusCodeAndHeader(res)
             res.end(data)
         })
+    }else if(req.url === '/contact-me' && req.method === 'GET'){
+        readFile('./contact-me.html', (err, data) => {
+            if(err){
+                errResponse(res)
+                return
+            }
+
+            setStatusCodeAndHeader(res)
+            res.end(data)
+        })
+    }
+    else {
+       readFile('./404.html', (err, data) => {
+        if(err){
+                errResponse(res)
+                return
+            }
+
+            setStatusCodeAndHeader(res)
+            res.end(data)
+       })
     }
 });
 
